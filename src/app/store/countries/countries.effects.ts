2279d6d53a7360b2f32976ae880baa9b9347ae0e
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-01-15 16:23:57 +0100

    All files in the store

---
+++ src/app/store/countries/countries.effects.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Effect, Actions} from '@ngrx/effects';
+
+import * as timelineActions from './countries.actions';
+import { TimelineService } from '../../services/timeline.service';
+import 'rxjs/add/operator/switchMap';
+
+@Injectable()
+export class timelineEffects {
+  constructor(
+    private actions$: Actions,
+    private service: TimelineService
+  ) {}
+
+  @Effect()
+  load$ = this.actions$
+    .ofType(timelineActions.LOAD_SITES)
+    .map((action: timelineActions.LoadSites) => action.payload)
+    .switchMap(() => this.service.getTimeline())
+    .map((sites) => new timelineActions.LoadSitesSuccess(sites));
+}
