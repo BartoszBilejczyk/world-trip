32965ad055235ad1972e9da67106a9d7a219c4bf
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-01-26 21:59:20 +0100

    Fixed deleted components in modules declarations

--- src/app/country/country-routing.module.ts
+++ src/app/country/country-routing.module.ts
@@ -1,33 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CountryComponent } from './country.component';
-import { BasicInfoComponent } from './basic-info/basic-info.component';
-import { LivingCostsComponent } from './living-costs/living-costs.component';
-import { BudgetComponent} from './budget/budget.component';
-import { SightseeingComponent} from './sightseeing/sightseeing.component';
 
 const countryRoutes: Routes = [
   {
     path: 'country',
-    component: CountryComponent,
-    children: [
-      {
-        path: 'basic-info',
-        component: BasicInfoComponent
-      },
-      {
-        path: 'living-cost',
-        component: LivingCostsComponent
-      },
-      {
-        path: 'budget',
-        component:  BudgetComponent
-      },
-      {
-        path: 'sightseeing',
-        component: SightseeingComponent
-      }
-    ]
+    component: CountryComponent
   }
 ];
 
--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -7,10 +7,6 @@ import { CountryRoutingModule } from './country-routing.module';
 import { MatButtonModule } from '@angular/material';
 
 import { CountryComponent } from './country.component';
-import { BasicInfoComponent } from './basic-info/basic-info.component';
-import { LivingCostsComponent } from './living-costs/living-costs.component';
-import { BudgetComponent } from './budget/budget.component';
-import { SightseeingComponent } from './sightseeing/sightseeing.component';
 import { CountryMapComponent } from './country-map/country-map.component';
 import { CountriesService } from '../services/countries.service';
 
@@ -23,18 +19,10 @@ import { CountriesService } from '../services/countries.service';
   ],
   declarations: [
     CountryComponent,
-    BasicInfoComponent,
-    LivingCostsComponent,
-    BudgetComponent,
-    SightseeingComponent,
     CountryMapComponent
   ],
   exports: [
     CountryComponent,
-    BasicInfoComponent,
-    LivingCostsComponent,
-    BudgetComponent,
-    SightseeingComponent,
     CountryMapComponent
   ],
   providers: [
